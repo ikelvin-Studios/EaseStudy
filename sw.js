3f71de1569c81e23af1c7c79811078c2a96818e4
Kelvin Morrison <kelvinninja1@gmail.com>
2020-05-24 11:20:35 +0000

    Restructure: Project Files & Directories + Expanded Service Worker

---
+++ sw.js
@@ -0,0 +1,60 @@
+const cacheName = 'EaseStudy-v0.3';
+
+// const staticAssets = [
+//   './',
+//   './includes/app.js',
+//   './includes/styles.css',
+//   './includes/fallback.json',
+//   './includes/images/fetch-dog.jpg'
+// ];
+
+const staticAssets = [
+  './',
+  './includes/app.js',
+  './includes/styles.css',
+  './includes/fallback.json',
+  './static/assets/vendor/bootstrap/css/bootstrap.min.css',
+  './static/assets/vendor/font-awesome/css/font-awesome.min.css',
+  './static/assets/vendor/linearicons/style.css',
+  './static/assets/vendor/metisMenu/metisMenu.css',
+  './static/assets/vendor/toastr/toastr.min.css',
+  './static/assets/css/main.css',
+
+  'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700'
+];
+
+self.addEventListener('install', async function () {
+  const cache = await caches.open(cacheName);
+  cache.addAll(staticAssets);
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  const url = new URL(request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request));
+  } else {
+    event.respondWith(networkFirst(request));
+  }
+});
+
+async function cacheFirst(request) {
+  const cachedResponse = await caches.match(request);
+  return cachedResponse || fetch(request);
+}
+
+async function networkFirst(request) {
+  const dynamicCache = await caches.open('news-dynamic');
+  try {
+    const networkResponse = await fetch(request);
+    dynamicCache.put(request, networkResponse.clone());
+    return networkResponse;
+  } catch (err) {
+    const cachedResponse = await dynamicCache.match(request);
+    return cachedResponse || await caches.match('./fallback.json');
+  }
+}
