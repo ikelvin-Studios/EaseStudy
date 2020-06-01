77de6c79acbff1b5b96a85ee8b4af8f4eeac06c8
Kelvin Morrison <kelvinninja1@gmail.com>
2020-06-01 16:51:00 +0000

    Updated: Service Worker To support the Complete offline first experience

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'EaseStudy-v0.3';
+const cacheName = 'EaseStudy-v1.0.1';
 
 // const staticAssets = [
 //   './',
@@ -9,25 +9,45 @@ const cacheName = 'EaseStudy-v0.3';
 // ];
 
 const staticAssets = [
-  // './',
+  './',
+  './index.html',
   './includes/app.js',
-  './includes/styles.css',
-  './includes/fallback.json',
+  './includes/main.js',
+  './pages/home.html',
+  './pages/quiz.html',
+  './static/assets/img/lazyload.gif',
+  './static/assets/img/loader.gif',
+  './static/assets/img/about.gif',
+  './static/assets/img/welcome.jpg',
   './static/assets/vendor/bootstrap/css/bootstrap.min.css',
   './static/assets/vendor/font-awesome/css/font-awesome.min.css',
   './static/assets/vendor/linearicons/style.css',
   './static/assets/vendor/metisMenu/metisMenu.css',
   './static/assets/vendor/toastr/toastr.min.css',
   './static/assets/css/main.css',
-
   'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700',
-
-  'static/assets/vendor/jquery/jquery.min.js',
-  'static/assets/vendor/bootstrap/js/bootstrap.min.js',
-  'static/assets/vendor/metisMenu/metisMenu.js',
-  'static/assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js',
-  'static/assets/vendor/toastr/toastr.js',
-  'static/assets/scripts/common.js'
+  './static/assets/vendor/jquery/jquery.min.js',
+  './static/assets/vendor/bootstrap/js/bootstrap.min.js',
+  './static/assets/vendor/metisMenu/metisMenu.js',
+  './static/assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js',
+  './static/assets/vendor/toastr/toastr.js',
+  './static/assets/scripts/common.js',
+  './contents/english/2017-obj.json',
+  './contents/english/2017-written.json',
+  './contents/english/2018-written.json',
+  './contents/english/2018-obj.json',
+  './contents/maths/2017-obj.json',
+  './contents/maths/2017-written.json',
+  './contents/maths/2018-written.json',
+  './contents/maths/2018-obj.json',
+  './contents/science/2017-obj.json',
+  './contents/science/2017-written.json',
+  './contents/science/2018-written.json',
+  './contents/science/2018-obj.json',
+  './contents/social/2017-obj.json',
+  './contents/social/2017-written.json',
+  './contents/social/2018-written.json',
+  './contents/social/2018-obj.json'
 ];
 
 self.addEventListener('install', async function () {
